78e49a534489c2530e8571f4e1c22b3bee3f4570
HeyIts-RJ <rishab1300@gmail.com>
2020-03-31 20:31:40 +0530

    Added Quarinitine Data API

--- app/src/main/java/com/developer/android/covid_19/SplashScreen.java
+++ app/src/main/java/com/developer/android/covid_19/SplashScreen.java
@@ -3,9 +3,13 @@ package com.developer.android.covid_19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -18,7 +22,11 @@ public class SplashScreen extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.rgb(227,227,227));
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
